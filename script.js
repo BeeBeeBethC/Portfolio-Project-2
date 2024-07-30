d0a3ffcadfd63c0c54c6d28a467b3f5890a45b2e
Bethan Cottell <cottellbc@gmail.com>
2024-07-30 15:37:57 +0000

    "everything stopped loading, reset links for CSS pages and JavaScript, added in divs on html and confirmed they work."

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("I am connected.");
+
+/*const sourceImages = [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+]
+const defaultImage = "default.png";
+
+const randomImages = [...sourceImages, ...sourceImages].sort((a, b) => Math.random() > 0.5 ? 1 : -1);
+
+console.log(randomImages);
+*/
