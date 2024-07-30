e43bffc632b10ebb91f026f49712ce9c88e768a8
Bethan Cottell <cottellbc@gmail.com>
2024-07-30 10:22:31 +0000

    "added and linked images to JavaScript, successfully generated array in dev tools console."

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello!")
+const sourceImages = [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+]
+const defaultImage = "default.png";
+
+const randomImage = [...sourceImages, ...sourceImages].sort((a, b) => Math.random() > 0.5 ? 1 : -1);
+
+console.log(randomImage);
