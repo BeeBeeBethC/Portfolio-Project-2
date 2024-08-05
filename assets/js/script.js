923e51d22d8e77e4e6f95af318f39a317442911c
Bethan Cottell <cottellbc@gmail.com>
2024-08-05 16:50:08 +0000

    "removed html comment to enable js to communicate"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
 const gameContainer = document.getElementById("game-container");
-const flipsHolder = document.querySelector('.flipsHolder').textContent = flips;
-const matchHolder = document.querySelector('.matchHolder').textContent = matches;
+const flipsHolder = document.querySelector('.flipsHolder').textContent = "Flips";
+const matchHolder = document.querySelector('.matchHolder').textContent = "Matches";
+let cards = [];
 let card1, card2;
 let chosenCards = [];
 let chosenCardsIds = [];
 let flips = 0;
 let matches = 0;
 
-fetch("./data/tile.json")
-    .then((res) => res.json())
-    .then((data) => {
-        cards = [...data, ...data];
-        cardShuffle();
-        generateBoard();
-    });
+console.log('Path:','./assets/data/tile.json');
 
-const cards = [...data, ...data];
+function fetch() {
+fetch(resource.json)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Unexpected error occurred:' + response.status);
+        }
+    }
+)}; 
+
+generateBoard();
+cardShuffle();
+
 document.getElementById("game-container").innerHTML = cards;
 
 function cardShuffle() {
@@ -25,10 +33,11 @@ function cardShuffle() {
         cards[i] = cards[j];
         cards[j] = k;
     }
-    document.getElementById("game-container").innerHTML = cards;
+
 }
 
 function generateBoard() {
+    document.getElementById("game-container").innerHTML = cards;
     for (let card of cards) {
         const cardsElement = document.createElement("div");
         cardsElement.classList.add("cards");
@@ -46,17 +55,18 @@ function generateBoard() {
     }
 }
 
-function checkForMatch() { // checks two cards selected and if both cards are the same the class matched is added
-    card1 = chosenCardsIds[0];
-    card2 = chosenCardsIds[1];
-    if (chosenCards[0] === chosenCards[1]) {
-        (document.querySelectorAll('img'))[card1].classList.add('matched');
-        (document.querySelectorAll('img'))[card2].classList.add('matched');
-    } else {
-        (chosenCards[0] !== chosenCards[1]); {
-            cards[card1].setAttribute('src', './assets/images/default.png');
-            cards[card2].setAttribute('src', './assets/images/default.png');
-            return;
+
+    function checkForMatch() { // checks two cards selected and if both cards are the same the class matched is added
+        card1 = chosenCardsIds[0];
+        card2 = chosenCardsIds[1];
+        if (chosenCards[0] === chosenCards[1]) {
+            (document.querySelectorAll('img'))[card1].classList.add('matched');
+            (document.querySelectorAll('img'))[card2].classList.add('matched');
+        } else {
+            (chosenCards[0] !== chosenCards[1]); {
+                cards[card1].setAttribute('src', './assets/images/default.png');
+                cards[card2].setAttribute('src', './assets/images/default.png');
+                return;
+            }
         }
     }
-}
